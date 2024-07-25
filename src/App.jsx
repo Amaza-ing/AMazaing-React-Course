6f7e845e3a49cbf9b839d09cb1a773ea95f4264b
Adrián Maza <adri_mv1@hotmail.com>
2024-07-25 19:16:44 +0200

    useEffect-component-lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import Child from "./components/Child";
 
 function App() {
+  const [showChild, setShowChild] = useState(true);
   return (
     <div>
       <h1>useEffect() | Ciclo de Vida de los Componentes</h1>
 
-      <Child></Child>
+      <button onClick={() => setShowChild(!showChild)}>Toggle Child</button>
+      {showChild && <Child></Child>}
     </div>
   );
 }
