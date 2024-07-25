4fecb5f62415ec640c5a6f886eeedfde5d8fa30a
Adrián Maza <adri_mv1@hotmail.com>
2024-07-25 19:46:58 +0200

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
-import Child from "./components/Child";
-
 function App() {
-  const [showChild, setShowChild] = useState(true);
   return (
     <div>
-      <h1>useEffect() | Ciclo de Vida de los Componentes</h1>
-
-      <button onClick={() => setShowChild(!showChild)}>Toggle Child</button>
-      {showChild && <Child></Child>}
+      <h1>fetch() | Llamada a una API</h1>
     </div>
   );
 }
