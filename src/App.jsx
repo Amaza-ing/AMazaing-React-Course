06661fd2b730cccc7b93a48451e726cecb27e050
Adrián Maza <adri_mv1@hotmail.com>
2024-07-25 20:06:36 +0200

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  const HTMLUsers = users.map((user) => {
+    return (
+      <div key={user.id}>
+        <h3>{user.name}</h3>
+      </div>
+    )
+  })
+
   return (
     <div>
       <h1>fetch() | Llamada a una API</h1>
+
+      <section>{HTMLUsers}</section>
     </div>
   );
 }
