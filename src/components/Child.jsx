edc236b849473ebcc7690e0414c9dc60097e6135
Adrián Maza <adri_mv1@hotmail.com>
2024-07-25 19:43:58 +0200

    useEffect-components-lifecycle

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 function Child() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    return () => {
+      console.log("El componente se ha desmontado");
+    }
+  }, [])
+
   return (
     <div className="child">
       <h2>Este es un componente hijo</h2>
+      <h3>{counter}</h3>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   );
 }
