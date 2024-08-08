d363f6cfbd475e57cdc8b2bcab0e7633e808dc46
Adrián Maza <adri_mv1@hotmail.com>
2024-08-08 18:56:04 +0200

    routes-fetch

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
 import HeaderComponent from "../components/HeaderComponent";
+import PostCard from "../components/PostCard";
 
 function BlogPage() {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const data = await response.json();
+    console.log(data);
+    setPosts(data);    
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
+  const postCards = posts.map((post) => {
+    return (
+      <li key={post.id}>
+        <PostCard post={post}></PostCard>
+      </li>
+    )
+  })
+
   return (
     <>
       <HeaderComponent></HeaderComponent>
       
-      <div>BlogPage</div>
+      <section>
+        <h2>Blog</h2>
+
+        <ul>{postCards}</ul>
+      </section>
     </>
   );
 }
