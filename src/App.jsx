c412e5b17ec0b979b3eb65a1110107d06208df34
Adrián Maza <adri_mv1@hotmail.com>
2024-08-08 16:41:04 +0200

    form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-import { useEffect } from "react";
-import { useState } from "react";
-
 function App() {
-  const [users, setUsers] = useState([]);
-
-  const getUsers = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    setUsers(data);
-  }
-
-  useEffect(() => {
-    getUsers();
-  }, [])
-
-  const HTMLUsers = users.map((user) => {
-    return (
-      <div key={user.id}>
-        <h3>{user.name}</h3>
-      </div>
-    )
-  })
-
   return (
     <div>
-      <h1>fetch() | Llamada a una API</h1>
+      <h1>
+        Formularios
+      </h1>
 
-      <section>{HTMLUsers}</section>
+      <form>
+        <fieldset>
+          <label htmlFor="username">Usuario: </label>
+          <input
+            type="text"
+            id="username"
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password">Contraseña: </label>
+          <input
+            type="password"
+            id="password"
+          />
+        </fieldset>
+        <button>Enviar</button>
+      </form>
     </div>
   );
 }
