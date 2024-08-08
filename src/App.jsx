23fcdd2d76f571c58eb365f980705d9cbcd39fa5
Adrián Maza <adri_mv1@hotmail.com>
2024-08-08 17:09:16 +0200

    forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+
 function App() {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(user);
+    console.log("El formulario se ha enviado");
+  };
+
+  const handleUsernameInput = (e) => {
+    setUser({ ...user, username: e.target.value });
+  };
+
+  const handlePasswordInput = (e) => {
+    setUser({ ...user, password: e.target.value });
+  };
+
   return (
     <div>
-      <h1>
-        Formularios
-      </h1>
+      <h1>Formularios</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <label htmlFor="username">Usuario: </label>
           <input
             type="text"
             id="username"
+            onChange={handleUsernameInput}
+            value={user.username}
           />
         </fieldset>
         <fieldset>
@@ -18,10 +40,16 @@ function App() {
           <input
             type="password"
             id="password"
+            onChange={handlePasswordInput}
+            value={user.password}
           />
         </fieldset>
         <button>Enviar</button>
       </form>
+
+      <button onClick={() => setUser({ username: "", password: "" })}>
+        Logout
+      </button>
     </div>
   );
 }
