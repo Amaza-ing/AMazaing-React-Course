bb5b08ed7c4f93811e7f6f7bce417ae686971b9b
Adrián Maza <adri_mv1@hotmail.com>
2024-09-24 12:14:06 +0200

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BlogPage from "./pages/BlogPage";
 import PostDetails from "./pages/PostDetails";
+import SettingsPage from "./pages/SettingsPage";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Route path="/" element={<HomePage />} />    
       <Route path="/blog" element={<BlogPage />} />    
       <Route path="/blog/:id" element={<PostDetails />} />    
+      <Route path="/settings" element={<SettingsPage />} />
+
+      <Route path="*" element={<ErrorPage />} />
+
     </Routes>
   );
 }
