2b8eb1bb78c276de5128761af581c7c13236c58f
Adrián Maza <adri_mv1@hotmail.com>
2024-09-27 16:55:27 +0200

    context-fetch-api

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { UserProviderWrapper } from "./contexts/user.context.jsx";
+import { BlogProviderWrapper } from "./contexts/blog.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <UserProviderWrapper>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </UserProviderWrapper>
+  <BlogProviderWrapper>
+    <UserProviderWrapper>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProviderWrapper>
+  </BlogProviderWrapper>
   // </React.StrictMode>,
 );
