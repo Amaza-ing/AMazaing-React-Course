20051ad18cdcc7a58d67e94cfe2949a83cd9956a
Adrián Maza <adri_mv1@hotmail.com>
2024-10-04 12:41:25 +0200

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
 import HomePage from "./pages/HomePage";
-import BlogPage from "./pages/BlogPage";
-import PostDetails from "./pages/PostDetails";
-import SettingsPage from "./pages/SettingsPage";
-import ErrorPage from "./pages/ErrorPage";
+
+const BlogPage = lazy(() => import("./pages/BlogPage"));
+const PostDetails = lazy(() => import("./pages/PostDetails"));
+const SettingsPage = lazy(() => import("./pages/SettingsPage"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />    
-      <Route path="/blog" element={<BlogPage />} />    
-      <Route path="/blog/:id" element={<PostDetails />} />    
-      <Route path="/settings" element={<SettingsPage />} />
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:id" element={<PostDetails />} />
+        <Route path="/settings" element={<SettingsPage />} />
 
-      <Route path="*" element={<ErrorPage />} />
-
-    </Routes>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
