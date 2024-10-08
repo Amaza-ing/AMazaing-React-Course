3f76583ec84e7ff18775af2365b15db5f83148e3
Adrián Maza <adri_mv1@hotmail.com>
2024-10-08 19:03:12 +0200

    testing-basics

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,13 +4,22 @@ import { UserContext } from "../contexts/user.context";
 
 function HomePage() {
   const { user, login, logout } = useContext(UserContext);
+  const [counter, setCounter] = useState(0);
 
   return (
     <>
       <HeaderComponent></HeaderComponent>
 
       <h2>Hola {user && <span>{user.name}</span>}</h2>
-      <div>HomePage</div>
+      <div data-testid="homepage-title">HomePage</div>
+
+      <br /> <br /> <br />
+      <h2>
+        <button onClick={() => setCounter(counter - 1)}>-</button>
+        <span data-testid="counter">{counter}</span>
+        <button onClick={() => setCounter(counter + 1)} data-testid="increase-counter">+</button>
+      </h2>
+      <br /> <br /> <br />
 
       <button onClick={() => login()}>Login</button>
       <button onClick={() => logout()}>Logout</button>
--- src/pages/HomePage.test.js
+++ src/pages/HomePage.test.js
@@ -1,7 +1,39 @@
-// import HomePage from "./HomePage";
+import { fireEvent, render } from "@testing-library/react";
+import HomePage from "./HomePage";
+import { UserProviderWrapper } from "../contexts/user.context";
+import { MemoryRouter } from "react-router-dom";
 
 describe("HomePage", () => {
   it("simple test", () => {
     expect(1).toEqual(1);
-  })
-})
+  });
+
+  it("should get HomePage as title", () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <UserProviderWrapper>
+          <HomePage />
+        </UserProviderWrapper>
+      </MemoryRouter>
+    );
+
+    const homePageTitle = getByTestId("homepage-title").textContent;
+    expect(homePageTitle).toEqual("HomePage");
+  });
+
+  it("should add one to counter", () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <UserProviderWrapper>
+          <HomePage />
+        </UserProviderWrapper>
+      </MemoryRouter>
+    );
+
+    const increseBtn = getByTestId("increase-counter");
+    fireEvent.click(increseBtn);
+
+    const counter = getByTestId("counter").textContent;
+    expect(counter).toEqual("1")
+  });
+});
