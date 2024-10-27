43479ad58d4d5eae7d92a3bd1c931b070028f5aa
Adrián Maza <adri_mv1@hotmail.com>
2024-10-27 12:18:33 +0100

    03-useState-variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 function App() {
-  const saySomething = () => {
-    console.log("something");    
-  }
+  // let number = 0;
+  const [number, setNumber] = useState(0);
+
+  const addOne = () => {
+    // number++;
+    setNumber(number + 1);
+    console.log(number);
+  };
 
   return (
     <div>
-      <h1 onClick={saySomething}>Hola a todos</h1>
-      {/* <h1 onClick={() => console.log("patata")}>Hola a todos</h1> */}
+      <h1>Hola a todos</h1>
+
+      <h2 onClick={addOne}>Number: {number}</h2>
     </div>
   );
 }
