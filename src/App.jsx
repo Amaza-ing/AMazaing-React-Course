74d1c92ab838537c113c4974f03490a6b12e9de0
Adrián Maza <adri_mv1@hotmail.com>
2024-10-27 12:59:54 +0100

    08-props-parent-to-child

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import { animals } from "./data/animals";
-
-const movies = ["Lord of the Rings", "Star Wars", "Dune"];
+import Child from "./components/Child";
 
 function App() {
-  const HTMLMovies = movies.map((movie, index) => {
-    return (
-      <p key={movie}>
-        {index + 1} - {movie}
-      </p>
-    );
-  });
-
-  const HTMLAnimals = animals.map((animal) => {
-    return (
-      <div key={animal.id}>
-        <h2>{animal.name}</h2>
-        <img src={animal.img} alt="" width={200} />
-      </div>
-    );
-  });
+  const text = "Variable desde el padre";
+  const person = {
+    sex: "hombre",
+    age: 30,
+  };
 
   return (
     <div>
-      <h1>Renderizado de Listas</h1>
+      <h1>Props | Comunicación entre componentes Padre-Hijo ⬇</h1>
 
-      {HTMLMovies}
-      {HTMLAnimals}
+      <Child msg={text} person={person}></Child>
     </div>
   );
 }
